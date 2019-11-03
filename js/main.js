4c1e5911984553454b56fc0507e4ca1a8cf01ca2
Aleksandr Khozhainov <sea.man.aleksandr@gmail.com>
2019-11-04 01:41:40 +0200

    01:41:33 Понедельник, 4 Ноябрь 2019 г

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('#menu-togle');
-const mobileNavContainer = document.querySelector('#mobile-nav')
-
-menuToggle.onclick = function(){
-    menuToggle.classList.toggle('menu-icon-active');
-    mobileNavContainer.classList.toggle('mobile-nav--active');
-}
